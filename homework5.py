7c1414ff2b6d80c0dbbaceee738d61d7c4c49d68
soneoko <Sofya.kayumova01@gmail.com>
2024-07-10 23:34:21 +0500

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,5 @@
+immutable_var = 1, 2, 'string', True
+print('Immutable tuple:',immutable_var) #элементы кортежа нельзя изменить, тк кортеж не поддерживает обращение по элементам
+mutable_list = [3, 4, 'sonya', False]
+mutable_list[0] = 8
+print('Mutable list:',mutable_list)
